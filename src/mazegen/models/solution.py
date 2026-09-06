204dbb45d9315f7b0bf6411fb01846b43199aed8
isa-macbook-pro <132603690+isaenesuslu@users.noreply.github.com>
2026-09-06 05:10:35 +0300

    feat(solution.py): add solution details(will be edit(path_cell))

--- src/mazegen/models/solution.py
+++ src/mazegen/models/solution.py
@@ -1,1 +1,13 @@
-# ADIM 4 (Temel) - Çözüm yolu verisi (Girişten çıkışa giden yol)
+from typing import List, Tuple
+from .direction import Direction
+
+class Solution:
+
+    def __init__(self, path_cells: List[Tuple[int, int]], directions: List[Direction]) -> None:
+
+        self.path_cells = path_cells
+
+        self.directions = directions
+
+    def to_string(self) -> str:
+        return " ".join([d.name[0] for d in self.directions])
