75b69a6bee45705840d82ff3af44dcba39a15fe1
isa-macbook-pro <132603690+isaenesuslu@users.noreply.github.com>
2026-09-09 22:58:02 +0300

    feat(init.py): add all algorithms and all

--- src/mazegen/generation/__init__.py
+++ src/mazegen/generation/__init__.py
@@ -1,7 +1,13 @@
 from .generator_base import GeneratorBase
-from .recursive_backtracker import Recursive_Backtracker
+from .recursive_backtracker import RecursiveBacktracker
+from .prim import PrimGenerator
+from .kruskal import KruskalGenerator
+from .braided import BraidedGenerator
 
 __all__ = [
-    'GenaratorBase',
+    'GeneratorBase',
     'RecursiveBacktracker',
+    'PrimGenerator',
+    'KruskalGenerator',
+    'BraidedGenerator',
 ]
