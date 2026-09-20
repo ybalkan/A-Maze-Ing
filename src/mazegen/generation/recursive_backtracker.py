2b53212c2506e6c5a3e3a8891de5f8ab2b601412
Yusuf Balkan <ybalkan@k2m17s03.42kocaeli.com.tr>
2026-09-20 21:18:40 +0300

    refactor(recursive_backtracker.py): finish recursive_backtracker

--- src/mazegen/generation/recursive_backtracker.py
+++ src/mazegen/generation/recursive_backtracker.py
@@ -1,40 +1,44 @@
 import random
+from typing import Optional, Tuple, Generator
 from mazegen.models.maze import Maze
 from mazegen.models.direction import Direction
+from mazegen.models.cell import Cell
 from mazegen.generation.generator_base import GeneratorBase
 
 
 class RecursiveBacktracker(GeneratorBase):
 
-    def generate(self) -> Maze:
-        maze = Maze(self.width, self.height)
+    def generate_steps(self) -> Generator[None, None, None]:
+        self.maze.apply_42_pattern()
+
         rng = random.Random(self.seed)
 
-        start = maze.grid[0][0]
+        start = self.maze.grid[0][0]
         start.visited = True
         stack = [start]
 
         while stack:
             current = stack[-1]
-            unvisited = self._unvisited_neighbors(maze, current, rng)
+
+            unvisited = self._unvisited_neighbors(self.maze, current, rng)
 
             if unvisited:
                 direction, neighbor = unvisited
-                maze.remove_wall_between(current, neighbor, direction)
+
+                self.maze.remove_wall_between(current, neighbor, direction)
+
                 neighbor.visited = True
                 stack.append(neighbor)
+                yield None
             else:
                 stack.pop()
-
-        maze.reset_visited_flags()
-        return maze
 
     def _unvisited_neighbors(
         self,
         maze: Maze,
-        cell: 'Cell',  # type: ignore[name-defined]
+        cell: Cell,
         rng: random.Random,
-    ) -> tuple | None:
+    ) -> Optional[Tuple[Direction, Cell]]:
         candidates = []
 
         for direction in Direction:
