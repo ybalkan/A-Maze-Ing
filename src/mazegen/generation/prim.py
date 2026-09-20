8afe51edad8470536fcb0f7c5073c3c77d2b42bf
Yusuf Balkan <ybalkan@k2m17s03.42kocaeli.com.tr>
2026-09-20 21:17:03 +0300

    refactor(prim.py): finish prim

--- src/mazegen/generation/prim.py
+++ src/mazegen/generation/prim.py
@@ -1,4 +1,5 @@
 import random
+from typing import Generator
 from mazegen.models.maze import Maze
 from mazegen.models.cell import Cell
 from mazegen.models.direction import Direction
@@ -7,15 +8,15 @@ from mazegen.generation.generator_base import GeneratorBase
 
 class PrimGenerator(GeneratorBase):
 
-    def generate(self) -> Maze:
-        maze = Maze(self.width, self.height)
+    def generate_steps(self) -> Generator[None, None, None]:
+        self.maze.apply_42_pattern()
         rng = random.Random(self.seed)
 
-        start = maze.grid[0][0]
+        start = self.maze.grid[0][0]
         start.visited = True
 
         frontier: list[tuple[Direction, Cell, Cell]] = []
-        self._add_frontier_walls(maze, start, frontier)
+        self._add_frontier_walls(self.maze, start, frontier)
 
         while frontier:
             idx = rng.randrange(len(frontier))
@@ -26,13 +27,10 @@ class PrimGenerator(GeneratorBase):
             if to_cell.visited:
                 continue
 
-            maze.remove_wall_between(from_cell, to_cell, direction)
+            self.maze.remove_wall_between(from_cell, to_cell, direction)
             to_cell.visited = True
-            self._add_frontier_walls(maze, to_cell, frontier)
-
-        maze.reset_visited_flags()
-        return maze
-
+            self._add_frontier_walls(self.maze, to_cell, frontier)
+            yield None
 
     def _add_frontier_walls(
         self,
