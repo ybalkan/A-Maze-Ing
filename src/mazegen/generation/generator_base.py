e609f417933ba57c95b065675242029b86937f4a
Yusuf Balkan <ybalkan@k2m17s03.42kocaeli.com.tr>
2026-09-20 21:15:50 +0300

    refactor(generator_base.py): finish generator_base

--- src/mazegen/generation/generator_base.py
+++ src/mazegen/generation/generator_base.py
@@ -1,13 +1,22 @@
 from abc import ABC, abstractmethod
+from typing import Generator
 from mazegen.models.maze import Maze
 
 
 class GeneratorBase(ABC):
+
     def __init__(self, width: int, height: int, seed: int | None = None) -> None:
         self.width = width
         self.height = height
         self.seed = seed
+        self.maze = Maze(self.width, self.height)
 
     @abstractmethod
-    def generate(self) -> Maze:
+    def generate_steps(self) -> Generator[None, None, None]:
         ...
+
+    def generate(self) -> Maze:
+        for _ in self.generate_steps():
+            pass
+        self.maze.reset_visited_flags()
+        return self.maze
